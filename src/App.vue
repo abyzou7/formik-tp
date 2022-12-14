2f85953fe78c3898dfa9cc8e4c4fc4bd5dbf1d49
Kassim Mohamed <kassim91000@gmail.com>
2022-12-14 12:29:56 +0100

    Initial commit and formik component

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <Formik :initial-values="initialValues" :validate="validate" @submit="handleSubmit" />
+</template>
+
+<script lang="ts">
+import Formik from "./components/Formik.vue"
+
+export default {
+  name: 'App',
+  components: {
+    Formik,
+  },
+  data() {
+    return {
+      initialValues: {
+        email: '',
+        password: '',
+      }
+    }
+  },
+  methods: {
+    validate(values) {
+      console.log(values)
+      const errors = {};
+
+      if (!values.email) {
+        errors.email = 'Email is required';
+      } else if (
+        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+      ) {
+        errors.email = 'Incorrect email format';
+      }
+
+      if (!values.password) {
+        errors.password = 'Password is required';
+      }
+      return errors;
+    },
+    handleSubmit(values, setIsSubmitting) {
+      console.log('Form submitted', values)
+      setTimeout(() => {
+        setIsSubmitting(false);
+      }, 2000)
+    }
+  }
+}
+</script>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Formik.vue
@@ -0,0 +1,70 @@
+<template>
+  <form class="Formik" @submit.prevent="handleSubmit">
+    <h1>Formik TP</h1>
+    <!-- Get every field -->
+    <div v-for="field in fields">
+      <label :for="field">{{ field }} : </label>
+      <input type="text" v-model="form[field]" :placeholder="field">
+      <!-- Show error if there is one -->
+      <p v-if="errors[field]">
+        <span style="color: red">
+        {{ errors[field] }}
+        </span>
+      </p>
+    </div>
+    <!-- Submit button display -->
+    <br/>
+    <button type="submit" :disabled="isSubmitting">Submit</button>
+  </form>
+</template>
+
+<script lang="ts">
+const isEmpty = (obj: Object) => Object.keys(obj).length === 0
+
+export default {
+  name: 'Formik',
+  props: {
+    initialValues: {
+      type: Object,
+      required: true
+    },
+    validate: {
+      type: Function,
+      required: true,
+    },
+  },
+  emits: ['submit'],
+  data() {
+    return {
+      form: {},
+      errors: {},
+      isSubmitting: false,
+    }
+  },
+  computed: {
+    fields() {
+      const fields = Object.keys(this.initialValues)
+
+      return fields
+    },
+  },
+  methods: {
+    setIsSubmitting(isSubmitting) {
+      this.isSubmitting = isSubmitting
+    },
+    handleSubmit() {
+      const values = { ...this.form }
+      this.errors = this.validate(values)
+
+      if (isEmpty(this.errors)) {
+        this.$emit('submit', values, this.setIsSubmitting)
+
+        this.isSubmitting = true;
+      }
+    },
+  },
+  created() {
+    this.form = { ...this.initialValues };
+  }
+}
+</script>
