9b785fcdbb08058fb262e4f11b2d4bd0ee7d42f9
chakil28 <chakil93120@gmail.com>
2022-12-14 12:47:31 +0100

    add field component

---
+++ src/components/Field.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+      <label :for="name">{{ name }}</label>
+      <input :id="name" :name="name" :type="type" :value="value" @input="handleChange" />
+      <div v-if="error">{{ error }}</div>
+    </div>
+  </template>
+  
+  <script lang="ts">
+  export default {
+    name: 'Field',
+    props: {
+      name: {
+        type: String,
+        required: true,
+      },
+      type: {
+        type: String,
+        default: 'text',
+      },
+      value: {
+        type: String,
+        default: '',
+      },
+      error: {
+        type: String,
+        default: '',
+      },
+    },
+    emits: ['change'],
+    methods: {
+      handleChange(event: Event) {
+          const target = event.target as HTMLInputElement
+          this.$emit('change', target.value)
+      },
+    },
+  }
+  
+  </script>
+  
