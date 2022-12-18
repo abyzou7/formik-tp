d1c413d6603886e3dca8abefa98303ed3babacda
Kassim Mohamed <kassim91000@gmail.com>
2022-12-18 22:22:03 +0100

    Field component

---
+++ src/components/Field.vue
@@ -0,0 +1,44 @@
+<template>
+    <!-- Field -->
+    <Component :is="as" v-bind="{
+      value,
+      onInput,
+    }">
+      <slot></slot>
+    </Component>
+  </template>
+  
+  <script lang="ts">
+  // Formik imports
+  import type { FormikValue, FormikValueGetter, FormikValueSetter } from '../../types/formik';
+  export default {
+    name: 'Field',
+    props: {
+      name: {
+        type: String,
+        required: true,
+      },
+      as: {
+        type: String,
+        default: 'input',
+      },
+    },
+    inject: ['getValueByName', 'setValueByName'],
+    data() {
+      return {
+        modelValue: null,
+      }
+    },
+    computed: {
+      value() {
+        return (this.getValueByName as FormikValueGetter)(this.name)
+      },
+    },
+    methods: {
+      onInput(event: InputEvent) {
+        const value: FormikValue = (event.target as HTMLInputElement).value
+        return (this.setValueByName as FormikValueSetter)(this.name, value)
+      },
+    }
+  }
+  </script>
